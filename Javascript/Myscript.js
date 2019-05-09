b052d47e165530044f0aed246fd3eb610a2f6a9a
marishimwe <49573950+marishimwe@users.noreply.github.com>
2019-05-09 15:27:31 +0200

    Create Myscript.js

---
+++ Javascript/Myscript.js
@@ -0,0 +1,15 @@
+function myFunction(){
+	var userName=document.getElementById("username").value;
+	var passWord=document.getElementById("password").value;
+	if (userName == "user" && passWord == "user" ){
+	alert("Login successfully");
+	window.location ="application form.html";
+	}
+	else if (userName =="admin" && passWord == "admin"){
+	alert("welcome admin");
+	window.location ="Verification.html";
+    }
+	else{document.getElementById("demo").innerHTML = "Invalid username or password";
+
+	}
+}
